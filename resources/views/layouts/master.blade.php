3ccfc10d0ea17ab7bd762f31560b30807b840f1f
yingjhen <s3a632059@student.ncut.edu.tw>
2019-11-26 14:20:06 +0800

    修正-Javascript區塊當中路徑

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,3 +1,5 @@
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -11,40 +13,23 @@
 
     <title>@yield('title')</title>
 
-
     <!-- Bootstrap Core CSS -->
     <link href="{{asset('css/bootstrap.min.css')}}" rel="stylesheet">
-    <!-- Theme CSS -->
-    <link href="{{asset('css/clean-blog.min.css')}}"
-          rel="stylesheet">
-    <!-- Custom Fonts -->
-    <link href="{{asset('css/font-awesome.min.css')}}"
-          rel="stylesheet" type="text/css">
 
     <!-- Theme CSS -->
-    <link href="css/clean-blog.min.css" rel="stylesheet">
+    <link href="{{asset('css/clean-blog.min.css')}}" rel="stylesheet">
 
     <!-- Custom Fonts -->
-    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="{{asset('css/font-awesome.min.css')}}" rel="stylesheet" type="text/css">
     <link href='http://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
     <link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
 
-
-
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-
-    <!-- jQuery -->
-    <script src="{{asset('js/jquery.min.js')}}"></script>
-    <!-- Bootstrap Core JavaScript -->
-
-    <script src="{{asset('js/bootstrap.min.js')}}"></script>
-    <!-- Contact Form JavaScript -->
-    <script src="{{asset('js/jqBootstrapValidation.js')}}"></script>
-    <script src="{{asset('js/contact_me.js')}}"></script>
-    <!-- Theme JavaScript -->
-    <script src="{{asset('js/clean-blog.min.js')}}"></script>
-
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
 
 </head>
 
@@ -52,29 +37,27 @@
 
 <!-- Navigation -->
 @include('layouts.partials.navigation')
+
 @yield('content')
-
-
-
 <hr>
 
 <!-- Footer -->
 @include('layouts.partials.footer')
 
 <!-- jQuery -->
-<script src="vendor/jquery/jquery.min.js"></script>
+<script src="{{asset('js/jquery.min.js')}}"></script>
 
 <!-- Bootstrap Core JavaScript -->
-<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+<script src="{{asset('js/bootstrap.min.js')}}"></script>
 
 <!-- Contact Form JavaScript -->
-<script src="js/jqBootstrapValidation.js"></script>
-<script src="js/contact_me.js"></script>
+<script src="{{asset('js/jqBootstrapValidation.js')}}"></script>
+<script src="{{asset('js/contact_me.js')}}"></script>
 
 <!-- Theme JavaScript -->
-<script src="js/clean-blog.min.js"></script>
+<script src="{{asset('js/clean-blog.min.js')}}"></script>
 
 </body>
 
-</html>
 
+</html>
